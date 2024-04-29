4d4e7328300b78d0e37f60f3cfc96923d238520e
Gustavo Silva <77128963+GustavoSilvaz@users.noreply.github.com>
2024-04-29 20:27:03 -0300

    aula1

--- Variáveis/aula1.py
+++ Variáveis/aula1.py
@@ -1,1 +1,23 @@
-print('Hello World!')
+#Variáveis
+
+idade = 18
+ano = 2002
+
+print(type(idade))
+print(type(ano))
+
+#Condições
+
+x = int(input("Digite o número: "))
+
+if x > 7:
+    print("Maior que 7")
+
+elif x == 7:
+    print("O valor é igual")
+
+else: x < 7
+print("Menor que 7")
+
+#Laços
+
