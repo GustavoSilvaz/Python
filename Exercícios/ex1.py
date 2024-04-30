dd9df10fa06eaa23d77007a55801539d3f71d51e
Gustavo Silva <77128963+GustavoSilvaz@users.noreply.github.com>
2024-04-29 21:35:49 -0300

    aula1 final

---
+++ Exercícios/ex1.py
@@ -0,0 +1,7 @@
+nome = (input("Digite seu nome: "))
+print("Olá {}, seja \nbem vindo!".format(nome))
+
+#posso usar o ".format(n1, n2, r)" 
+#e posso repetir os valores".format(n1, n2, r, r, r)"
+#"\n" quebra linha
+#"{:.2f}".format(r) -> 2 casas decimais
