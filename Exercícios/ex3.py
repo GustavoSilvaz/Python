b59a8f7ae13059a05ec2b74589f36201242c9db1
Gustavo Silva <guh_sz@outlook.com>
2024-05-01 00:12:05 -0300

    tabuada
    com anotação da duda

--- Exercícios/ex3.py
+++ Exercícios/ex3.py
@@ -2,9 +2,21 @@
 #inicial = 1
 #while inicial <= 10:
 #    print("{}".format(digito * inicial))
+
 digito = int(input("Digite um número entre 1 e 9: "))
 i = 1
 while i < 11:
     resultado = digito * i
     print("{} x {} = {}".format(digito, i, resultado))
     i += 1
+
+""" valor = int(input("informe um numero: "))
+aux = 0
+
+= print("" 18)
+print("tabuada de {}".format(valor))
+print("" 18)
+
+while(aux <= 10):
+    print("{0} x {1} = {2}".format(valor, aux, valor * aux))
+    aux = aux + 1  """
